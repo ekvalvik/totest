23fe8f96145d5c1f449dbcec69a8ad704f3bdb2d
ekvalvik <71800987+ekvalvik@users.noreply.github.com>
2021-04-14 21:48:50 -0700

    Update ChaptTen.java

--- ChaptTen.java
+++ ChaptTen.java
@@ -1,39 +1,39 @@
-//@EvanKvalvik
 
 import java.util.*;
 import java.lang.*;
 
 public class ChaptTen {
   public static void main(String[]args){
- /*   
+ 
     ArrayList<String> hWA = new ArrayList<String>();
     hWA.add("hello");
     hWA.add("World");
-    //hWA.add("Again");
-    //hWA.add("For");
-    //hWA.add("The");
-    //hWA.add("Second");
-    //hWA.add("Time");
-    
+    hWA.add("Again");
+    hWA.add("For");
+    hWA.add("The");
+    hWA.add("Second");
+    hWA.add("Time");
+  
+  
     ArrayList<Integer> digits = new ArrayList<Integer>();
+    digits.add(1);
     digits.add(2);
-    digits.add(4);
     digits.add(5);
     digits.add(1);
     digits.add(3);
  
    doubleList(hWA); //doubles each element
-   //minToFront(digits); //puts minimum number to front of list
-  // stutter(3, hWA); //dublicates each element (int) many times.
-   //filterRange(digits, 3, 4); //removes numbers between first int and second int.
+  minToFront(digits); //puts minimum number to front of list
+  stutter(3, hWA); //dublicates each element (int) many times.
+   filterRange(digits, 3, 4); //removes numbers between first int and second int.
    System.out.println(hWA);
-  // System.out.println(digits);
- */ 
-  }
+  System.out.println(digits);
+}
+
   public static void doubleList(ArrayList<String> hWA){ //10.4
-    if(hWA== null)
+    if(hWA== null){
 		throw new IllegalArgumentException("ArrayList parameter Can't be null");
-    
+    }
     int size = hWA.size();
     int i;
     int j = 0; 
@@ -44,9 +44,14 @@ public class ChaptTen {
     }
   }
   public static void minToFront(ArrayList<Integer> digits){ //10.6
-     if (digits == null) {
+     if(digits.contains(null)) {
         throw new IllegalArgumentException("Parameter 'digits' cannot be null");
     }
+    if(digits == null) {
+        throw new IllegalArgumentException("tsk tsk");
+    }
+     
+		 
     
     int last = 0;
       for(int i = 0; i < digits.size()-1; i++){
@@ -54,10 +59,8 @@ public class ChaptTen {
                last = i;
       }
       int digit = digits.get(last);
-      try{
       digits.remove(last);
-      digits.add(0, digit);
-      } else {  throw new IllegalArgumentException("Parameter must not be imutable.");
+      digits.add(0, digit);   
    }
     public static void stutter(int k, ArrayList<String> hWA){ //10.11
      if (hWA == null){
